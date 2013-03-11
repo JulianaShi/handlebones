6af3395480da8f53b93d9d3de7f786ad3c272d3c
Juliana Shimer <jshimer@walmart.com>
2013-03-11 11:40:11 -0700

    Second draft Assignment 1 not hooked into backbone or handlebars yet...sorting and filtering implemented

---
+++ television.js
@@ -0,0 +1,95 @@
+$(function () {
+    $("#slider").slider({
+        range:true,
+        min:0,
+        max:80,
+        values:[ 20, 40 ],
+        slide:function (event, ui) {
+              var sliderMin = ui.values[ 0 ]
+              var sliderMax = ui.values[ 1 ]
+
+            $("#amount").html('"' + sliderMin + ' - "' + sliderMax);
+            $.fn.filterAndSort(items, sliderMin, sliderMax);
+        }
+    });
+    $("#amount").html('"' + $("#slider").slider("values", 0) +
+        ' - "' + $("#slider").slider("values", 1));
+    $('#tv-brand').change(function (e) {
+        $.fn.filterAndSort(items);
+    });
+    $('#tv-types').change(function (e) {
+        $.fn.filterAndSort(items);
+    });
+    $('#tv-sort').change(function (e) {
+        $.fn.filterAndSort(items);
+    });
+    $.fn.filterAndSort(items);
+    $('span.stars').stars(items);
+});
+
+$("#clear-filter").click(function () {
+    alert("clicked");
+})
+
+$.fn.stars = function () {
+    return $(this).each(function () {
+        // Get the value
+        var val = parseFloat($(this).html());
+        val = Math.round(val * 2) / 2;
+        /* To round to nearest half */
+        // Make sure that the value is in 0 - 5 range, multiply to get width
+        var size = Math.max(0, (Math.min(5, val))) * 16;
+        // Create stars holder
+        var $span = $('<span />').width(size);
+        // Replace the numerical value with stars
+        $(this).html($span);
+    });
+};
+
+$.fn.filterAndSort = function (items, minSize, maxSize) {
+    // if not passed in args then get default vals
+    if (arguments.length == 1){
+        minSize = $("#slider").slider("values", 0);
+        maxSize = $("#slider").slider("values", 1);
+    }
+    // If dropdown is a selection use it otherwise set to null
+    var type = ($("#tv-types").val() == "Select"? "" : $("#tv-types").val());
+    var brand = ($("#tv-brand").val() == "Select"? "" : $("#tv-brand").val());
+    var sort = $("#tv-sort").val();
+    console.log("minSize " +minSize +", maxSize " +maxSize + "brand " +brand +"type " +type + "sort " +sort )
+    var finalArray = [];
+    for (var i = 0; i < items.length; i++) {
+        var item = items[i];
+        // apply all filters
+        if ((item.size >= minSize && item.size <= maxSize)
+             && new RegExp('\\b' + brand + '\\b').test(item.name)
+             && new RegExp('\\b' + type + '\\b').test(item.name)
+            ){
+            finalArray.push(item);
+        }
+    }
+    // apply sorting by price or alphabetical
+    if (sort == "Price"){
+        finalArray.sort(function(a,b) { return a.size - b.size } );
+
+    } else {
+        finalArray.sort(function(a,b) {
+            var nameA = a.name;
+            var nameB = b.name;
+            if (nameA < nameB){
+                return -1;
+            }
+            if (nameB > nameA){
+                return 1
+            }
+            return 0;
+        });
+    }
+    // clear html template
+    $( "#results").html("")
+    //apply json to template and append results
+    $( "#resultsTemplate" ).tmpl( finalArray )
+        .appendTo( "#results" );
+
+}
+
