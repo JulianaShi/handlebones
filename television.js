a643ef552969672acd28e65aa0292dde605776a7
Juliana Shimer <jshimer@walmart.com>
2013-03-11 17:12:30 -0700

    changes to sort options and footer formatting

--- television.js
+++ television.js
@@ -5,8 +5,8 @@ $(function () {
         max:80,
         values:[ 20, 40 ],
         slide:function (event, ui) {
-              var sliderMin = ui.values[ 0 ]
-              var sliderMax = ui.values[ 1 ]
+            var sliderMin = ui.values[ 0 ]
+            var sliderMax = ui.values[ 1 ]
 
             $("#amount").html('"' + sliderMin + ' - "' + sliderMax);
             $.fn.filterAndSort(items, sliderMin, sliderMax);
@@ -30,19 +30,16 @@ $(function () {
 
     $.fn.filterAndSort(items);
 
-    $('span.stars').stars(items);
 
     $("#clear-filter").click(function () {
         $("tv-types").val("Select");
         $("tv-brand").val("Select");
-        $("#slider").slider("values", [20, 40] );
+        $("#slider").slider("values", [20, 40]);
         $("#amount").html('"' + $("#slider").slider("values", 0) +
             ' - "' + $("#slider").slider("values", 1));
         $.fn.filterAndSort(items);
     });
 });
-
-
 
 
 $.fn.stars = function () {
@@ -60,74 +57,64 @@ $.fn.stars = function () {
     });
 };
 
-$.fn.sortByNameAZ = function (a, b) {
-    var nameA = a.name.toLowerCase();
-    var nameB = b.name.toLowerCase();
-    if (nameA < nameB) {
-        return -1;
-    }
-    if (nameB > nameA) {
-        return 1
-    }
-    return 0;
+
+$.fn.sortByPriceLow = function (a, b) {
+    return a.price - b.price
 };
-
-$.fn.sortByNameZA = function (a, b) {
-    var nameA = a.name.toLowerCase();
-    var nameB = b.name.toLowerCase();
-    if (nameA < nameB) {
-        return 1;
-    }
-    if (nameB > nameA) {
-        return -1
-    }
-    return 0;
+$.fn.sortByPriceHigh = function (a, b) {
+    return b.price - a.price
 };
-
-
-$.fn.sortByPrice = function(a,b) { return a.price - b.price };
-
+$.fn.sortByRating = function (a, b) {
+    return b.rating - a.rating
+};
+$.fn.sortByScreenSize = function (a, b) {
+    return a.size - b.size
+};
 
 $.fn.filterAndSort = function (items, minSize, maxSize) {
     // if not passed in args then get default vals
-    if (arguments.length == 1){
+    if (arguments.length == 1) {
         minSize = $("#slider").slider("values", 0);
         maxSize = $("#slider").slider("values", 1);
     }
     // If dropdown is a selection use it otherwise set to null
-    var type = ($("#tv-types").val() == "Select"? "" : $("#tv-types").val());
-    var brand = ($("#tv-brand").val() == "Select"? "" : $("#tv-brand").val());
+    var type = ($("#tv-types").val() == "Select" ? "" : $("#tv-types").val());
+    var brand = ($("#tv-brand").val() == "Select" ? "" : $("#tv-brand").val());
     var sort = $("#tv-sort").val();
-    console.log("minSize " +minSize +", maxSize " +maxSize + "brand " +brand +"type " +type + "sort " +sort )
+//    console.log("minSize " +minSize +", maxSize " +maxSize + "brand " +brand +"type " +type + "sort " +sort )
     var finalArray = [];
     for (var i = 0; i < items.length; i++) {
         var item = items[i];
         // apply all filters
         if ((item.size >= minSize && item.size <= maxSize)
-             && new RegExp('\\b' + brand + '\\b').test(item.name)
-             && new RegExp('\\b' + type + '\\b').test(item.name)
-            ){
+            && new RegExp('\\b' + brand + '\\b').test(item.name)
+            && new RegExp('\\b' + type + '\\b').test(item.name)
+            ) {
             finalArray.push(item);
         }
     }
     // apply sorting by price or alphabetical
-    if (sort == "Price"){
-        finalArray.sort($.fn.sortByPrice);
+    if (sort == "LowestPrice") {
+        finalArray.sort($.fn.sortByPriceLow);
 
     }
-    if (sort == "AZ") {
-        finalArray.sort($.fn.sortByNameAZ);
+    if (sort == "HighestPrice") {
+        finalArray.sort($.fn.sortByPriceHigh);
 
     }
-    if (sort == "ZA") {
-        finalArray.sort($.fn.sortByNameZA);
+    if (sort == "TopRated") {
+        finalArray.sort($.fn.sortByRating);
+
+    }
+    if (sort == "ScreenSize") {
+        finalArray.sort($.fn.sortByScreenSize);
 
     }
     // clear html template
-    $( "#results").html("")
+    $("#results").html("")
     //apply json to template and append results
-    $( "#resultsTemplate" ).tmpl( finalArray )
-        .appendTo( "#results" );
-
+    $("#resultsTemplate").tmpl(finalArray)
+        .appendTo("#results");
+    $('span.stars').stars();
 }
 
